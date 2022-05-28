e83805098e050aff3540c67d0226d1f81053b692
ouesahlem <102174962+ouesahlem@users.noreply.github.com>
2022-05-29 00:59:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -88,28 +88,6 @@ async function sendEventToGorse(event: PluginEvent, meta: SendEventsPluginMeta) 
 				})
 	}
 	throw error
-	
-	//fetch
-        /*await fetch(
-                    url,
-                    {
-			method: method_type,
-                        headers: {
-                            'accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        },
-                    body: data
-                        
-                    }
-                ).then((response) => response.json())
-				//Then with the data from the response in JSON...
-				.then((data) => {
-				console.log('Success:', data);
-				})
-				//Then with the error genereted...
-				.catch((error) => {
-				  console.error('Error:', error);
-				})*/
 	    
     } else {
         
